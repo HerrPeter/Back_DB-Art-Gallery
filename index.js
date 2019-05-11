4d4eae60d36e2026a595123ecbeeb5466e2c2c09
Peter Kaufinger <peterkaufinger@gmail.com>
2019-05-10 21:40:16 -0700

    Update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 //import mysql from 'mysql';
 const mysql = require('mysql');
+const express = require('express');
+const bodyParser = require('body-parser');
+var app = express();
 
+app.use(bodyParser.json());
 
 const connection = mysql.createConnection({
     host: '35.222.200.150',
@@ -16,4 +20,19 @@ connection.connect((err) => {
     } else {
         console.log('Successfully connected to DB.');
     }
+})
+
+app.listen(3000, () => {
+    console.log('Express Server running at port 3000.')
+});
+
+app.get('/artists', (res, req) => {
+    connection.query('SELECT * FROM Artist', (err, rows, fields) => {
+        if (err) {
+            console.log('Error getting artists.');
+            console.log(`Error: ${err}`);
+        } else {
+            console.log(rows);
+        }
+    })
 })
