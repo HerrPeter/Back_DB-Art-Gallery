5edc23a8fb1f15d04034beebed9c082c52fb0a25
Peter K <peterkaufinger@gmail.com>
2019-05-11 06:08:08 +0000

    Works locally not externally

--- index.js
+++ index.js
@@ -2,9 +2,22 @@
 const mysql = require('mysql');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Not working
 var app = express();
 
 app.use(bodyParser.json());
+
+app.listen(4000, () => {
+    console.log('Express Server running at port 4000.')
+});
+
+// --
+var http = require('http');
+http.createServer(function(req, res) {
+    res.end('Hi there.');
+}).listen(3030);
+//--
+
 
 const connection = mysql.createConnection({
     host: '35.222.200.150',
@@ -22,17 +35,20 @@ connection.connect((err) => {
     }
 })
 
-app.listen(3000, () => {
-    console.log('Express Server running at port 3000.')
-});
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
 
-app.get('/artists', (res, req) => {
+app.get('/artists', (req, res) => {
     connection.query('SELECT * FROM Artist', (err, rows, fields) => {
         if (err) {
             console.log('Error getting artists.');
             console.log(`Error: ${err}`);
         } else {
+            console.log('Success...');
             console.log(rows);
         }
-    })
+    });
+
+    res.send('Artists.');
 })
