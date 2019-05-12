f6435e61d709447dd52ebb1c87455b192a7e2508
Peter Kaufinger <peterk_@yahoo.com>
2019-05-11 22:50:21 -0700

    Updated server

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
-var http = require('http');
-var express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
 
-var app = express();
-app.use(bodyParser.json());
+const app = express();
+app.use(cors());
 
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!!');
-}).listen(8080);
+const connection = mysql.createConnection({
+    host: '35.222.200.150',
+    user: 'sample',
+    password: 'sample',
+    database: 'ArtGallery'
+})
 
-app.listen(80, () => {
-    console.log('Express Server running at port 3000.')
-});
+connection.connect((err) => {
+    if (err) {
+        console.log('Error connecting to DB.');
+        console.log(`Error: ${JSON.stringify(err, undefined, 2)}`);
+    } else {
+        console.log('Successfully connected to DB.');
+    }
+})
 
-http.get('/', (req, res) => {
-    res.send('No way jose!');
+app.get('/', (req, res) => {
+    //res.send('Welcome Home Baby!');
+    var results = "My My What progress.";
+    return res.json({
+        data: results
+    });
+})
+
+app.listen(4000, () => {
+    console.log('Server now listening on port: 4000');
 })
