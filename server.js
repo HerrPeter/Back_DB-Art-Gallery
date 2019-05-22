19f6ff6187c7e01bb5224f80806e9005e1bafd2d
Peter Kaufinger <peterk_@yahoo.com>
2019-05-22 00:36:11 -0700

    Added adding to /artists directory

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+// -- My stuff -- //
+const {
+    MYSQL_DB
+} = require('./misc/cred');
+
 // -- Other stuff -- //
 const express = require('express');
 const cors = require('cors');
@@ -7,11 +12,8 @@ const path = require('path')
 const app = express();
 app.use(cors());
 
-// -- My stuff -- //
-const {
-    MYSQL_DB
-} = require('./misc/cred');
 
+// -- SQL Connection -- //
 // Create connection string.
 const connection = mysql.createConnection(MYSQL_DB)
 
@@ -33,7 +35,26 @@ app.get('/', (req, res) => {
     });
 })
 
-// Begin listening for client requests (start server).
+// Respond to /artists directory (w/ query values).
+app.get('/artists', (req, res) => {
+    // Parse request params.
+    var artists = '<NULL>';
+    const {name, phone, address, birthplace, age} = req.query;
+
+    // Make query request from SQL connection.
+    connection.query(`SELECT * FROM Artist WHERE name="${name}";`, (err, rows, fields) => {
+        if(err){
+            console.log(err);
+        }else{
+            console.log('Successful Query');
+            artists = rows;
+            console.log(artists);
+            return res.json({data: artists, columns: fields});
+        }
+    })
+})
+
+// -- Begin listening for client requests (start server) -- //
 app.listen(4000, () => {
     console.log('Server now listening on port: 4000');
 })
